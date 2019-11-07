be1510b25dda6fa7060005e28e2225387acb8de3
Pavel Tankovich <p.tankovich@andersenlab.com>
2019-11-07 20:36:19 +0300

    learn JSX

--- src/index.js
+++ src/index.js
@@ -5,32 +5,49 @@ const AppHeader = () => {
     return (
         <h1>Hello World!</h1>
     );
-}
+};
 
 const TodoList = (params) => {
+    const items = ['Learn React', 'Build Awesome App'];
+
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Duild Awesome App</li>
+            <li>{ items[0] }</li>
+            <li>{ items[1] }</li>
         </ul>
     );
-}
+};
 
 const SearchPanel = () => {
+
+    const searchText = 'Type here to search';
+    const searchStyle ={
+        fontSize: '25px'
+    }
+
     return (
-        <input placeholder="Search" />
+        <input
+            style={ searchStyle }
+            placeholder={ searchText }
+            />
     );
-}
+};
 
 const App = () => {
+
+    const isLoggedIn = true;
+    const loginBox = <span>Log in please</span>;
+    const welcomBox = <span>Welcom!</span>;
+
     return (
         <div>
+            { isLoggedIn ? welcomBox : loginBox }
             <AppHeader />
             <SearchPanel />
             <TodoList />
         </div>
     );
-}
+};
 
 ReactDOM.render(<App />,
     document.querySelector('#root'));
