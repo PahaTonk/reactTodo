ede98e7a46547708b7745f79b7e1d44fd4c5309e
Pavel Tankovich <p.tankovich@andersenlab.com>
2019-11-08 15:35:28 +0300

    app transfer

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+
+import './app.css';
+
+import AppHeader from './../app-header';
+import TodoList from './../todo-list';
+import SearchPanel from './../search-panel';
+import ItemStatusFilter from './../item-status-filter';
+
+const App = () => {
+
+    const todoData = [
+        { label: 'Drink Coffee', id: 'dc' },
+        { label: 'Make Awesome App', important: true, id: 'maa' },
+        { label: 'Have a lunch', id: 'hl' }
+    ];
+
+    return (
+        <div className="todo-app">
+            <AppHeader toDo = {1} done = {3}/>
+            <div className="todo-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+
+            <TodoList todos = { todoData }/>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './css/libs/bootstrap.min.css';
-import './css/index.css';
+import './css/bootstrap.min.css';
 
-import AppHeader from './components/app-header';
-import TodoList from './components/todo-list';
-import SearchPanel from './components/search-panel';
-import ItemStatusFilter from './components/item-status-filter';
-
-const App = () => {
-
-    const todoData = [
-        { label: 'Drink Coffee', id: 'dc' },
-        { label: 'Make Awesome App', important: true, id: 'maa' },
-        { label: 'Have a lunch', id: 'hl' }
-    ];
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo = {1} done = {3}/>
-            <div className="todo-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-
-            <TodoList todos = { todoData }/>
-        </div>
-    );
-};
+import App from './components/app';
 
 ReactDOM.render(<App />,
     document.querySelector('#root'));
