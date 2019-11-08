121aa19794a210f0f49d7db6f77be3f053e080c8
Pavel Tankovich <p.tankovich@andersenlab.com>
2019-11-08 18:04:27 +0300

    sateful/stateless component

--- src/components/todo-list/todo-list-item/todo-list-item.js
+++ src/components/todo-list/todo-list-item/todo-list-item.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './../todo-list.css';
 
 import './../../../libs/fotnawesome';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const TodoListItem = ({ label, important }) => {
-    const itemStyles = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-      };
+export default class TodoListItem extends Component {
 
-    return (
-        <span className="todo-list-item">
-        <span
-            className="todo-list-item-label"
-            style={itemStyles}>
-            {label}
-        </span>
+    render () {
+        const { label, important } = this.props;
 
-        <button type="button"
-            className="btn btn-outline-success btn-sm float-right">
+        const itemStyles = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+          };
 
-            <FontAwesomeIcon icon="exclamation" />
-        </button>
 
-          <button type="button"
-                  className="btn btn-outline-danger btn-sm float-right">
-                <FontAwesomeIcon icon="trash" />
-          </button>
-        </span>
-      );
-};
+        return (
+            <span className="todo-list-item">
+                <span
+                    className="todo-list-item-label"
+                    style={itemStyles}>
+                    {label}
+                </span>
 
-export default TodoListItem;
+                <button type="button"
+                    className="btn btn-outline-success btn-sm float-right">
+                    <FontAwesomeIcon icon="exclamation" />
+                </button>
+
+                <button type="button"
+                    className="btn btn-outline-danger btn-sm float-right">
+                    <FontAwesomeIcon icon="trash" />
+                </button>
+            </span>
+          );
+    };
+}
