da757e9bfe3aa6334ca953f5574b431c139f371d
Pavel Tankovich <p.tankovich@andersenlab.com>
2019-11-10 23:12:34 +0300

    added new Item

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,20 +3,51 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (event) => {
+        const targetValue = event.target.value;
+
+        this.setState({
+            label: targetValue
+        });
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        const formInputSearch = event.currentTarget.querySelector('.todo-input-search');
+
+        if (!formInputSearch.value) return;
+
+        formInputSearch.value = '';
+
+        this.setState({
+            label: ''
+        });
+        this.props.onAddListItem(this.state.label);
+    }
 
     render () {
-        const { onAddListItem } = this.props;
-        const newItemLabel = 'Drink Coffee';
-
         return (
-            <div className = 'item-add-form'>
+            <form
+                className = 'item-add-form d-flex'
+                onSubmit = { this.onSubmit }
+            >
+                <input
+                    type = 'text'
+                    className = 'form-control todo-input-search'
+                    onChange = { this.onLabelChange }
+                    placeholder = 'What needs to be done'
+                />
                 <button
-                    onClick = { () => onAddListItem(newItemLabel) }
                     className = 'btn btn-outline-secondary'
                 >
                     Add Item
                 </button>
-            </div>
+            </form>
         );
     };
 };
